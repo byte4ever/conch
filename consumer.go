237559dd646ee0b6095fde5fc3395180b321f96f
byte4ever <laurent8martin@gmail.com>
2023-05-27 16:19:55 +0200

    perfect test coverage for consumer

--- consumer.go
+++ consumer.go
@@ -2,22 +2,62 @@ package conch
 
 import (
 	"context"
-	"runtime/debug"
+	"errors"
+	"runtime"
+	"strconv"
+	"strings"
 	"sync"
+)
+
+var (
+	ErrInvalidConsumerPoolCount = errors.New("invalid consumer pool count")
+	ErrNilConsumerDoer          = errors.New("nil consumer doer")
+	ErrNilConsumerPoolDoer      = errors.New("nil consumer pool doer doer")
 )
 
 func intercept[T any](
 	logger Logger,
 	f Doer[T],
 ) Doer[T] {
+	const (
+		MaxCallerStakSize = 128
+		StringBufferSize  = 2048
+	)
+
 	return func(ctx context.Context, t T) {
 		defer func() {
 			if r := recover(); r != nil {
+				var sb strings.Builder
+
+				sb.Grow(StringBufferSize)
+
+				stk := make([]uintptr, MaxCallerStakSize)
+				n := runtime.Callers(4, stk)
+				stk = stk[:n]
+
+				for _, pc := range stk {
+					f := runtime.FuncForPC(pc)
+					file, l := f.FileLine(pc)
+					name := f.Name()
+
+					if strings.Contains(name, "conch.intercept[...].func1") {
+						break
+					}
+
+					sb.WriteString(name)
+					sb.WriteString("\n\t")
+					sb.WriteString(file)
+					sb.WriteRune(':')
+					sb.WriteString(strconv.Itoa(l))
+					sb.WriteRune('\n')
+				}
+
+				err, _ := r.(error)
 				logger.Error(
-					"intercept panic",
+					"conch intercepts panic",
 					map[string]any{
-						"error": r,
-						"stack": string(debug.Stack()),
+						"error": err.Error(),
+						"stack": sb.String(),
 					},
 				)
 			}
@@ -27,6 +67,14 @@ func intercept[T any](
 	}
 }
 
+// Consumer spawn a single consumer for the input stream.
+//
+// It's shut down when the input stream is closed or the context is
+// canceled and wg is used to wait for the consumer to finish when shutdown
+// condition is met.
+//
+// Panics that may occur during doer execution will be caught and
+// logged on global logger.
 func Consumer[T any](
 	ctx context.Context,
 	wg *sync.WaitGroup,
@@ -43,6 +91,10 @@ func consumer[T any](
 	f Doer[T],
 	inStream <-chan T,
 ) {
+	if f == nil {
+		panic(ErrNilConsumerDoer)
+	}
+
 	wg.Add(1)
 
 	go func() {
@@ -64,6 +116,14 @@ func consumer[T any](
 	}()
 }
 
+// ConsumerPool spawns n concurrent  consumers for the input stream.
+//
+// All of them are shut down when the input stream is closed or the context is
+// canceled and wg is used to wait for all consumers to finish when shutdown
+// condition is met.
+//
+// Panics that may occur during doer execution will be caught and
+// logged on global logger.
 func ConsumerPool[T any](
 	ctx context.Context,
 	wg *sync.WaitGroup,
@@ -71,8 +131,27 @@ func ConsumerPool[T any](
 	f Doer[T],
 	inStream <-chan T,
 ) {
+	consumerPool(ctx, wg, logger, count, f, inStream)
+}
+
+func consumerPool[T any](
+	ctx context.Context,
+	wg *sync.WaitGroup,
+	l Logger,
+	count int,
+	f Doer[T],
+	inStream <-chan T,
+) {
+	if count <= 0 {
+		panic(ErrInvalidConsumerPoolCount)
+	}
+
+	if f == nil {
+		panic(ErrNilConsumerPoolDoer)
+	}
+
 	for i := 0; i < count; i++ {
-		Consumer(ctx, wg, f, inStream)
+		consumer(ctx, wg, l, f, inStream)
 	}
 }
 
--- consumer_test.go
+++ consumer_test.go
@@ -7,15 +7,37 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kr/pretty"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
-	"go.uber.org/zap"
-	zapadapter "logur.dev/adapter/zap"
 )
 
-func TestConsumer(t *testing.T) {
+// f1 is paniking when x = 2
+//
+//go:noinline
+func f1(x int) {
+	f2(x)
+}
+
+//go:noinline
+func f2(x int) {
+	f3(x)
+}
+
+//go:noinline
+func f3(x int) {
+	f4(x)
+}
+
+//go:noinline
+func f4(x int) {
+	x += 1
+	x -= 1
+	fmt.Println(123123123 / (x - 2))
+}
+
+func Test_consumer(t *testing.T) {
 	t.Parallel()
 
 	t.Run(
@@ -92,15 +114,36 @@ func TestConsumer(t *testing.T) {
 
 			l := NewMockLogger(t)
 			l.On(
-				"PanicError",
-				"intercept panic",
+				"Error",
+				"conch intercepts panic",
 				mock.MatchedBy(
 					func(m map[string]any) bool {
-						fmt.Println("================")
-						pretty.Println(m)
-						fmt.Println("================")
-
-						return true
+						return assert.Contains(
+							// error key MUST be present
+							t,
+							m,
+							"error",
+						) && assert.Equal(
+							// error message MUST match original message
+							t,
+							"runtime error: integer divide by zero",
+							m["error"],
+						) && assert.Contains(
+							// stack key MUST be present
+							t,
+							m,
+							"stack",
+						) && assert.Contains(
+							// MUST contain the function name that panics
+							t,
+							m["stack"],
+							".f4",
+						) && assert.Contains(
+							// MUST contain the file name and  panics locaion
+							t,
+							m["stack"],
+							"/consumer_test.go:37",
+						)
 					},
 				),
 			)
@@ -121,10 +164,8 @@ func TestConsumer(t *testing.T) {
 					2,
 				).
 				Run(
-					func(args mock.Arguments) {
-						k := 0
-						fmt.Println(120 / k)
-						// panic("mocked panic")
+					func(mock.Arguments) {
+						f1(2)
 					},
 				).
 				Return(nil).
@@ -161,38 +202,830 @@ func TestConsumer(t *testing.T) {
 			)
 		},
 	)
-}
 
-func Test_A(t *testing.T) {
-	localLogger, err := zap.NewDevelopment()
-	require.NoError(t, err)
-	ReplaceLogger(zapadapter.New(localLogger))
-	defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
-	var wg sync.WaitGroup
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
 
-	inStream := make(chan int, 3)
-	inStream <- 1
-	inStream <- 2
-	inStream <- 3
-	close(inStream)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	Consumer(
-		ctx,
-		&wg,
-		func(ctx context.Context, x int) {
-			fmt.Println(123123123 / (x - 2))
+			l := NewMockLogger(t)
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			consumer(
+				ctx,
+				&wg,
+				l,
+				d.Execute,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
 		},
-		inStream,
 	)
 
-	// cancel()
-	require.Eventually(
-		t, func() bool {
-			wg.Wait()
-			return true
-		}, time.Second, time.Millisecond,
+	t.Run(
+		"panicking when doer is nil", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			require.PanicsWithValue(
+				t,
+				ErrNilConsumerDoer,
+				func() {
+					consumer(
+						ctx,
+						&wg,
+						l,
+						nil,
+						inStream,
+					)
+				},
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+}
+
+const consumerPoolSize = 10
+
+func Test_consumerPool(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"closing input", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			consumerPool(
+				ctx,
+				&wg,
+				l,
+				consumerPoolSize,
+				d.Execute,
+				inStream,
+			)
+
+			// cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"panic processing", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+			l.On(
+				"Error",
+				"conch intercepts panic",
+				mock.MatchedBy(
+					func(m map[string]any) bool {
+						return assert.Contains(
+							// error key MUST be present
+							t,
+							m,
+							"error",
+						) && assert.Equal(
+							// error message MUST match original message
+							t,
+							"runtime error: integer divide by zero",
+							m["error"],
+						) && assert.Contains(
+							// stack key MUST be present
+							t,
+							m,
+							"stack",
+						) && assert.Contains(
+							// MUST contain the function name that panics
+							t,
+							m["stack"],
+							".f4",
+						) && assert.Contains(
+							// MUST contain the file name and  panics locaion
+							t,
+							m["stack"],
+							"/consumer_test.go:37",
+						)
+					},
+				),
+			)
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Run(
+					func(mock.Arguments) {
+						f1(2)
+					},
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			consumerPool(
+				ctx,
+				&wg,
+				l,
+				consumerPoolSize,
+				d.Execute,
+				inStream,
+			)
+
+			// cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			consumerPool(
+				ctx,
+				&wg,
+				l,
+				consumerPoolSize,
+				d.Execute,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"panicking when count is invalid edge case", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			require.PanicsWithValue(
+				t,
+				ErrInvalidConsumerPoolCount,
+				func() {
+					consumerPool(
+						ctx,
+						&wg,
+						l,
+						0,
+						d.Execute,
+						inStream,
+					)
+				},
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"panicking when doer is nil", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			l := NewMockLogger(t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			require.PanicsWithValue(
+				t,
+				ErrNilConsumerPoolDoer,
+				func() {
+					consumerPool(
+						ctx,
+						&wg,
+						l,
+						10,
+						nil,
+						inStream,
+					)
+				},
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+}
+
+func Test_Consumer(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"closing input", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			Consumer(
+				ctx,
+				&wg,
+				d.Execute,
+				inStream,
+			)
+
+			// cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			Consumer(
+				ctx,
+				&wg,
+				d.Execute,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+}
+
+func Test_ConsumerPool(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"closing input", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			ConsumerPool(
+				ctx,
+				&wg,
+				consumerPoolSize,
+				d.Execute,
+				inStream,
+			)
+
+			// cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			ConsumerPool(
+				ctx,
+				&wg,
+				consumerPoolSize,
+				d.Execute,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"panicing when count is invalid edge case", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			require.PanicsWithValue(
+				t,
+				ErrInvalidConsumerPoolCount,
+				func() {
+					ConsumerPool(
+						ctx,
+						&wg,
+						0,
+						d.Execute,
+						inStream,
+					)
+				},
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+}
+
+func Test_ConsumerPoolC(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"closing input", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			ConsumerPoolC(
+				consumerPoolSize,
+				d.Execute,
+			)(
+				ctx,
+				&wg,
+				inStream,
+			)
+
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			ConsumerPoolC(
+				consumerPoolSize,
+				d.Execute,
+			)(
+				ctx,
+				&wg,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"panicking when count is invalid edge case", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			require.PanicsWithValue(
+				t,
+				ErrInvalidConsumerPoolCount,
+				func() {
+					ConsumerPoolC(
+						0,
+						d.Execute,
+					)(
+						ctx,
+						&wg,
+						inStream,
+					)
+				},
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+}
+
+func Test_ConsumerC(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"closing input", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+			d.
+				On(
+					"Execute",
+					ctx,
+					1,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					2,
+				).
+				Return(nil).
+				Once()
+			d.
+				On(
+					"Execute",
+					ctx,
+					3,
+				).
+				Return(nil).
+				Once()
+
+			inStream := make(chan int, 3)
+			inStream <- 1
+			inStream <- 2
+			inStream <- 3
+			close(inStream)
+
+			ConsumerC(
+				d.Execute,
+			)(
+				ctx,
+				&wg,
+				inStream,
+			)
+
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
+	)
+
+	t.Run(
+		"cancel kills the processor", func(t *testing.T) {
+			t.Parallel()
+
+			defer goleak.VerifyNone(t, goleak.IgnoreCurrent())
+			var wg sync.WaitGroup
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			d := NewMockDoer[int](t)
+
+			inStream := make(chan int)
+			defer close(inStream)
+
+			ConsumerC(
+				d.Execute,
+			)(
+				ctx,
+				&wg,
+				inStream,
+			)
+
+			cancel()
+			require.Eventually(
+				t, func() bool {
+					wg.Wait()
+					return true
+				}, time.Second, time.Millisecond,
+			)
+		},
 	)
 }
