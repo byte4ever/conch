1f790737d0dbffe2891819eb17eb2267f9e05474
byte4ever <laurent8martin@gmail.com>
2023-10-23 20:02:30 +0200

    impl counts

---
+++ count.go
@@ -0,0 +1,55 @@
+package conch
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+func Count[T any](
+	ctx context.Context,
+	counter *atomic.Uint64,
+	inStream <-chan T,
+) <-chan T {
+	outStream := make(chan T)
+
+	go func() {
+		defer close(outStream)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case in, more := <-inStream:
+				if !more {
+					return
+				}
+
+				select {
+				case outStream <- in:
+					counter.Add(1)
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}()
+
+	return outStream
+}
+
+func CountsC[T any](
+	counter *atomic.Uint64,
+	chain ChainsFunc[T],
+) ChainsFunc[T] {
+	return func(
+		ctx context.Context,
+		wg *sync.WaitGroup,
+		inStreams ...<-chan T,
+	) {
+		for _, inStream := range inStreams {
+			chain(ctx, wg, Count(ctx, counter, inStream))
+		}
+	}
+}
